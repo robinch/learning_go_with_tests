45cbb24a55f327b2c348e6f56407b0c460014829
Robin Chowdhury <robinch88@gmail.com>
2023-12-04 22:05:48 +0100

    structs

---
+++ stucts/shapes_test.go
@@ -0,0 +1,34 @@
+package structs
+
+import "testing"
+
+func TestParameter(t *testing.T) {
+	got := Parameter(Rectangle{10.0, 10.0})
+	want := 40.0
+
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
+
+func TestArea(t *testing.T) {
+	areaTest := []struct {
+		name  string
+		shape Shape
+		want  float64
+	}{
+		{name: "Rectangle", shape: Rectangle{Width: 12.0, Height: 6.0}, want: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10}, want: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, want: 36.0},
+	}
+
+	for _, tt := range areaTest {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+
+			if got != tt.want {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.want)
+			}
+		})
+	}
+}
